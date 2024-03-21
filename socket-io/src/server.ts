c8168044ff7d015e2840bc174b066217791ad5e9
m4rc3l04ugu2t0 <mrcelo014@gmail.com>
2024-03-20 23:46:19 -0300

    node

---
+++ socket-io/src/server.ts
@@ -0,0 +1,97 @@
+import express from 'express'
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
+
+const app = express()
+const server = createServer(app)
+const socketIo = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+interface Player {
+  id: string
+  socket: Socket
+  status: string
+}
+
+interface SessionGameProps {
+  [key: string]: Socket[]
+}
+
+let players: Player[] = []
+const sessionGame: SessionGameProps = {}
+
+socketIo.on('connection', (socket) => {
+  console.log('connected')
+
+  players.push({
+    id: socket.id,
+    socket,
+    status: 'online'
+  })
+
+  socket.on('disconnect', () => {
+    console.log('disconnected')
+    players = players.filter((player) => player.id !== socket.id)
+  })
+
+  socket.on('lookingFor', (message) => {
+    console.log('lookingFor', message)
+    lookingFor(socket)
+  })
+
+  socket.on('sendMessageRoom', (roomId: string) => {
+    console.log('roomID ', roomId)
+    const salas = socketIo.sockets.adapter.rooms
+    console.log('Salas e usuários nelas:')
+    salas.forEach((usuarios, salaId) => {
+      console.log(`Sala ${salaId}:`)
+      usuarios.forEach((usuarioId) => {
+        console.log(`- Usuário ${usuarioId}`)
+      })
+    })
+    socketIo.to(roomId).emit('newMessage', { roomId })
+  })
+})
+
+function lookingFor(player: Socket) {
+  updateStatus(player.id, 'lookingFor')
+  let playersLookingFor = players.filter(
+    (player) => player.status === 'lookingFor'
+  )
+
+  if (playersLookingFor.length <= 1) return
+
+  const roomId: string = playersLookingFor[0].id + playersLookingFor[1].id
+
+  sessionGame[roomId] = [
+    playersLookingFor[0].socket,
+    playersLookingFor[1].socket
+  ]
+
+  playersLookingFor[0].socket.join(roomId)
+  playersLookingFor[1].socket.join(roomId)
+
+  playersLookingFor[0].socket.emit('playing', roomId)
+  playersLookingFor[1].socket.emit('playing', roomId)
+
+  updateStatus(playersLookingFor[0].socket.id, 'playing')
+  updateStatus(playersLookingFor[1].socket.id, 'playing')
+  playersLookingFor = []
+}
+
+function updateStatus(playerId: string, status: string) {
+  players = players.map((player: Player) => {
+    if (player.id === playerId) {
+      return { ...player, status }
+    } else {
+      return player
+    }
+  })
+}
+
+server.listen(3000, () => {
+  console.log('server running')
+})
---
+++ socket-io/src/shuffler.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const shuffle = async () => {
+  const response = await axios.get(
+    'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2'
+  )
+
+  return await response.data
+}
