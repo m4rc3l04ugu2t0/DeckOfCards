932c1e466a6f9b784c016c01f615c4df4a9a8c48
m4rc3l04ugu2t0 <mrcelo014@gmail.com>
2024-03-20 22:22:03 -0300

    room playings

--- socket.io/src/server.ts
+++ socket.io/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { createServer } from 'http'
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 
 const app = express()
 const server = createServer(app)
@@ -10,19 +10,89 @@ const socketIo = new Server(server, {
   }
 })
 
+interface Player {
+  id: string
+  socket: Socket
+  status: string
+}
+
+interface SessionGameProps {
+  [key: string]: Socket[]
+}
+
+let players: Player[] = []
+const sessionGame: SessionGameProps = {}
+
 socketIo.on('connection', (socket) => {
   console.log('connected')
 
-  socket.on('disconnect', () => {
-    console.log('disconncted')
+  players.push({
+    id: socket.id,
+    socket,
+    status: 'online'
   })
 
-  socket.on('play', (message) => {
-    // socketIo.emit('play', 'server')
-    socket.broadcast.emit('play', 'playing...')
-    console.log('playing', message)
+  socket.on('disconnect', () => {
+    console.log('disconnected')
+    players = players.filter((player) => player.id !== socket.id)
+  })
+
+  socket.on('lookingFor', (message) => {
+    console.log('lookingFor', message)
+    lookingFor(socket)
+  })
+
+  socket.on('sendMessageRoom', (roomId: string) => {
+    console.log('roomID ', roomId)
+    const salas = socketIo.sockets.adapter.rooms
+    console.log('Salas e usuários nelas:')
+    salas.forEach((usuarios, salaId) => {
+      console.log(`Sala ${salaId}:`)
+      usuarios.forEach((usuarioId) => {
+        console.log(`- Usuário ${usuarioId}`)
+      })
+    })
+    socketIo.to(roomId).emit('newMessage', roomId)
   })
 })
+
+function lookingFor(player: Socket) {
+  updateStatus(player.id, 'lookingFor')
+  let playersLookingFor = players.filter(
+    (player) => player.status === 'lookingFor'
+  )
+
+  console.log(playersLookingFor.length)
+
+  if (playersLookingFor.length <= 1) return
+
+  const roomId: string = playersLookingFor[0].id + playersLookingFor[1].id
+
+  sessionGame[roomId] = [
+    playersLookingFor[0].socket,
+    playersLookingFor[1].socket
+  ]
+
+  playersLookingFor[0].socket.join(roomId)
+  playersLookingFor[1].socket.join(roomId)
+
+  playersLookingFor[0].socket.emit('playing', roomId)
+  playersLookingFor[1].socket.emit('playing', roomId)
+
+  updateStatus(playersLookingFor[0].socket.id, 'playing')
+  updateStatus(playersLookingFor[1].socket.id, 'playing')
+  playersLookingFor = []
+}
+
+function updateStatus(playerId: string, status: string) {
+  players = players.map((player: Player) => {
+    if (player.id === playerId) {
+      return { ...player, status }
+    } else {
+      return player
+    }
+  })
+}
 
 server.listen(3000, () => {
   console.log('server running')
