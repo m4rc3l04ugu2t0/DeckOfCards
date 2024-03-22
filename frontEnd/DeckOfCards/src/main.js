5e0d89f0d879cce1069034fb287d83f302339d3b
Marcelo <mrcelo014@gmail.com>
2024-03-21 23:15:22 -0300

    player disconnected room

--- frontEnd/DeckOfCards/src/main.js
+++ frontEnd/DeckOfCards/src/main.js
@@ -5,9 +5,20 @@ import { player2 } from './players/player2'
 
 const socket = connectionSocket()
 let player
+let onlines
 
 socket.on('playing', (message) => {
   sendMessageServer('sendMessageRoom', message)
+})
+
+socket.on('playersOnline', (message) => {
+  onlines = message
+  document.getElementById('onlines').textContent = message
+})
+
+socket.on('playerDisconnected', (message) => {
+  alert(message)
+  player2()
 })
 
 socket.on('newMessage', (message) => {
@@ -25,5 +36,6 @@ const sendMessageServer = (type, contentMsg) => {
 }
 
 document.getElementById('play').onclick = function lookingForCorrespondence() {
+  if (onlines <= 1) return alert('players onlines insuficiente')
   sendMessageServer('lookingFor', player)
 }
--- frontEnd/DeckOfCards/src/players/player2.js
+++ frontEnd/DeckOfCards/src/players/player2.js
@@ -1,6 +1,21 @@
 const cardsPlayer2 = document.getElementById('player2')
 
-export const player2 = async (cards) => {
+export const player2 = async (cards = false) => {
+  if (!cards) {
+    cardsPlayer2.innerHTML = `
+        <img src="https://www.deckofcardsapi.com/static/img/back.png" alt="Verso da Carta" class="w-36 h-46">
+        <img src="https://www.deckofcardsapi.com/static/img/back.png" alt="Verso da Carta" class="w-36 h-46">
+        <img src="https://www.deckofcardsapi.com/static/img/back.png" alt="Verso da Carta" class="w-36 h-46">
+        <img src="https://www.deckofcardsapi.com/static/img/back.png" alt="Verso da Carta" class="w-36 h-46">
+        <img src="https://www.deckofcardsapi.com/static/img/back.png" alt="Verso da Carta" class="w-36 h-46">
+        <img src="https://www.deckofcardsapi.com/static/img/back.png" alt="Verso da Carta" class="w-36 h-46">
+        <img src="https://www.deckofcardsapi.com/static/img/back.png" alt="Verso da Carta" class="w-36 h-46">
+        <img src="https://www.deckofcardsapi.com/static/img/back.png" alt="Verso da Carta" class="w-36 h-46">
+        <img src="https://www.deckofcardsapi.com/static/img/back.png" alt="Verso da Carta" class="w-36 h-46">
+    `
+
+    return
+  }
   const images = []
 
   cards.cards.map((card) => {
